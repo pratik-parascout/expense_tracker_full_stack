ba192ea82f765a864c76ffadcb9fce4090ed0a9d
Pratik Kumar <pratikumar56@gmail.com>
2024-12-11 13:53:04 +0530

    add bcrypt to hash the user password and made the required changes to the signup and login pages accordingly

--- controller/login.js
+++ controller/login.js
@@ -1,27 +1,34 @@
 const path = require('path');
+
+const bcrypt = require('bcrypt');
 const User = require('../model/User');
 
 exports.getLogin = (req, res) => {
   res.sendFile(path.join(__dirname, '../public/html/login.html'));
 };
 
-exports.postLogin = (req, res) => {
+exports.postLogin = async (req, res) => {
   const { email, password } = req.body;
 
-  User.findOne({ where: { email: email } })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ msg: 'User Not Found' });
-      }
+  try {
+    const user = await User.findOne({ where: { email } });
 
-      if (user.password === password) {
-        return res.status(200).json({ msg: 'User Login Successful.' });
-      } else {
-        return res.status(401).json({ msg: 'User not authorized' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ msg: 'Internal Server Error' });
-    });
+    if (!user) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    console.log('User Login successful');
+    res.status(200).json({ msg: 'Login successful' });
+  } catch (err) {
+    console.error('Error during login:', err);
+    res
+      .status(500)
+      .json({ msg: 'An error occurred during login. Please try again.' });
+  }
 };
--- controller/signup.js
+++ controller/signup.js
@@ -1,35 +1,36 @@
 const path = require('path');
+
+const bcrypt = require('bcrypt');
 const User = require('../model/User');
 
 exports.getSignup = (req, res) => {
   res.sendFile(path.join(__dirname, '../public/html/index.html'));
 };
 
-exports.postSignup = (req, res) => {
+exports.postSignup = async (req, res) => {
   const { username, email, password } = req.body;
 
-  User.findOne({ where: { email } })
-    .then((existingUser) => {
-      if (existingUser) {
-        res.status(400).json({ msg: 'User already exists' });
-        return null;
-      }
-      return User.create({
-        username,
-        email,
-        password,
-      });
-    })
-    .then((newUser) => {
-      if (newUser) {
-        console.log('User Created:', newUser);
-        res.redirect('/');
-      }
-    })
-    .catch((err) => {
-      console.error('Error during signup:', err);
-      res
-        .status(500)
-        .json({ msg: 'An error occurred during signup. Please try again.' });
+  try {
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
     });
+
+    console.log('User Created:', newUser);
+    res.redirect('/signup');
+  } catch (err) {
+    console.error('Error during signup:', err);
+    res
+      .status(500)
+      .json({ msg: 'An error occurred during signup. Please try again.' });
+  }
 };
